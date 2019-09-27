a3cfb73089c13c37a7324e65fba3e2165db7eca3
Pamela <pamablang@gmail.com>
2019-09-27 14:11:41 +1000

    Updates

---
+++ precache-manifest.621ed59c98b5c70426099e8c57ad3207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe14deafd476eae07a527769afa79451",
+    "url": "/pmdb/index.html"
+  },
+  {
+    "revision": "f149b8048dea30c691ec",
+    "url": "/pmdb/static/css/main.e62f1059.chunk.css"
+  },
+  {
+    "revision": "0019436f4e1be2ef08cc",
+    "url": "/pmdb/static/js/2.ca9b3eff.chunk.js"
+  },
+  {
+    "revision": "f149b8048dea30c691ec",
+    "url": "/pmdb/static/js/main.3728deb9.chunk.js"
+  },
+  {
+    "revision": "6730bf893d1789f7d02c",
+    "url": "/pmdb/static/js/runtime~main.f55c2a61.js"
+  }
+]);
